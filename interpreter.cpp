4aa093a45688c37178342b6c3d1516b748e78a35
NurizAite <56302424+NurizAite@users.noreply.github.com>
2020-02-15 11:47:54 +0600

    Add files via upload

--- interpreter.cpp
+++ interpreter.cpp
@@ -15,10 +15,11 @@ class Number : public Lexem {
 	int value ;
 public:
 	Number () {};
-	Number (int value) {
+	Number (int num) {
+		value = num;
 		cout << "constructor value "<< value << endl;
 	};
-//	void print() {cout << value << endl;};
+	void print() {cout << value << endl;};
 	int getValue ();
 };
 
@@ -39,12 +40,15 @@ int PRIORITY [] = {
 class Oper: public Lexem {
 	OPERATOR opertype ;
 public:
-//	Oper () {};
+	Oper () {};
 	Oper (string str) {
+	//	cout << 
+		if (str == "+")
+			opertype = PLUS;
 		cout << "string constructor" << endl;
 	}
 	OPERATOR getType ();
-//	void print() {cout << str << endl;}
+	void print() {cout << opertype << endl;}
 	int getPriority ();
 	int getValue (const Number & left,
                       const Number & right);
@@ -63,18 +67,16 @@ vector<Lexem *> parseLexem(
 			j = i;
 			while (input[j] >= '0' && input[j] <= '9') {
 				j++;
-			//	value = value + (input[i] - '0');
 			}
 			value = 0;
 			while (i < j) {
 				value = value + (input[i] - '0') * pow(10, j - i - 1);
 				i++;
 			}
-		//	i++;
 			cout << value << endl;
-		//	cout << i << endl;
 			infix.push_back(new Number(value));
-//			infix.print(value);
+		//	infix.push_back(new Number(value));
+		//	infix.print();
 		}
 		else {
 			cout << "*operator*" << endl;
@@ -83,13 +85,13 @@ vector<Lexem *> parseLexem(
 			while ((input[i] < '0' || input[i] > '9') && input[i] != ' ') {
 				i++;
 				count++;
-		//		cout << "index " << i << endl;
 			}
 			opr = input.substr(j, count);
-			cout << "oper is "<< opr << endl;
+			cout << "oper is " << opr << endl;
 			infix.push_back(new Oper(opr));
 		}
 	}
+	infix.push_back(NULL);
 	return infix;
 };
 
@@ -109,8 +111,10 @@ int main () {
 	getline(cin, codeline);
 //	while (getline(cin, codeline)) {
 		infix = parseLexem (codeline);
-//		infix.print();
-/*		for (auto i = infix.begin(); i != infix.end(); 
+		for (int i = 0; infix[i] != NULL; i++) {
+			infix[i] -> print();
+		}
+/*		for (auto i = infix.begin(); i != infix.end();
 ++i);
 			cout << *i << ' ';*/
 //		postfix = buildPostfix (infix);
